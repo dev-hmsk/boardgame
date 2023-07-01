4e97444f53a82b1093998187d62a1a2c23d7555f
dev-hmsk <dev.hmsk@gmail.com>
2023-07-01 15:42:54 -0400

    Added _generate_visual() to represent board state in terminal

--- board/board.py
+++ board/board.py
@@ -5,19 +5,32 @@ class Board():
         self.x_coor = dimension[0]
         self.y_coor = dimension[1]
         self.xy_coor = self._generate_dimension()
+        self.visual = self._generate_visual()
 
     def _generate_dimension(self):
         board_coor = {}
-
         for x_coor in range(1, self.x_coor + 1):
             for y_coor in range(1, self.y_coor + 1):
-                xy_coor = ("x" + str(x_coor) + "y" + str(y_coor))
+                xy_coor = (x_coor,y_coor)
                 board_coor[xy_coor] = None
         return board_coor
-
+    
+    def _generate_visual(self): # Iterate through all keys and represent their values visually. (1,1) starts at top left
+        visual_object = ""
+        for y in range(1, self.y_coor + 1):
+            for x in range(1, self.x_coor + 1):
+                xy_coor = (x, y)
+                if self.xy_coor[xy_coor] is None:
+                    visual_object += "[ ]"
+                else:
+                    visual_object += "[*]"
+            visual_object += "\n"
+        return visual_object
+        
     def _place(self, xy_coor, item):
         if self.xy_coor[xy_coor] is None:  # If space is empty, place item and return True
             self.xy_coor[xy_coor] = item
+            self.visual = self._generate_visual()
             return True, (f"Valid move, {item} has been placed at {xy_coor}")
 
         else: # If space is full, return False
@@ -27,7 +40,7 @@ class Board():
         return self.xy_coor[xy_coor]
 
     def remove(self, xy_coor): # Remove item at location if able
-        if self.xy_coor[xy_coor] is None:
+        if self.xy_coor[xy_coor]is None:
             return False, ("Nothing is here")
 
         if self.xy_coor[xy_coor] is not None:  # Something is here, return True and the piece at this location and set x,y to None
@@ -35,7 +48,7 @@ class Board():
             self.xy_coor[xy_coor] = None
             return True, piece_to_return, (f"{piece_to_return} is located here. Removing {piece_to_return}")
 
-    def move(self, xy_coor, item):
+    def move(self, xy_coor, item): # This is one way to do it, but i could also use the above in get + remove to first check if a move if valid
         check = self._place(xy_coor, item)
 
         if check[0] is True:
@@ -44,5 +57,11 @@ class Board():
         if check[0] is False:
             print(check[1])
 
-    def get_state(self):
+    def get_state(self): # Gives me the dictionary in all its glory
         return self.xy_coor
+    
+    def clear_board(self):
+        for all_keys in self.xy_coor:
+            self.xy_coor[all_keys] = None
+            self.visual = self._generate_visual()
+
--- main.py
+++ main.py
@@ -6,15 +6,19 @@ test_dim = (5, 5)
 board_1 = Board(test_dim)
 test_item1 = "test_obj_1"
 test_item2 = "test_obj_2"
-test_move1 = "x1y1"
-test_move2 = "x1y2"
+test_move1 = (1,1)
+test_move2 = (1,2)
 
+print(board_1.visual)
+board_1.move(test_move1, test_item1)
 
-print(board_1.move(test_move1, test_item1))
-print(board_1.get_state())
+board_1.move(test_move2, test_item2)
+board_1.move((3,3),"hewo")
+print(board_1.visual)
+board_1.move((4,4),"uwu")
+board_1.move((4,4),"whats")
+board_1.move((1,2),"this")
+print(board_1.visual)
 
-print(board_1.move(test_move1, test_item1))
-print(board_1.get_state())
-
-print(board_1.remove(test_move1))
-print(board_1.get_state())
+board_1.clear_board()
+print(board_1.visual)
