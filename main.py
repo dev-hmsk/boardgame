64d18dcc25fda1540d078030eb5bf5ae938b6ef9
dev-hmsk <dev.hmsk@gmail.com>
2023-07-05 08:55:20 -0400

    Abstracted player_turn() for better readability and modularity

--- main.py
+++ main.py
@@ -85,52 +85,62 @@ def capture_piece(capturable_moves, piece):
         print("Invalid Move. Try Again")
         make_valid_move(capturable_moves, piece)
 
+
+def select_piece(pieces):
+    selected_piece = cycle_through_pieces(pieces)
+    print(selected_piece.name, selected_piece.xy_coord)
+    return selected_piece
+
+
+def check_capturable_moves(selected_piece, team):
+    can_this_move = checkers_board.is_regular_move_valid(selected_piece)
+    print("Debug for capture")
+    print(can_this_move)
+
+    capturable_moves = {}
+    for move in can_this_move:
+        if can_this_move[move] is False:
+            check_space = selected_piece.moves[move]
+            if check_space is not None:
+                opp_piece = checkers_board.get_from_location(check_space)
+                if opp_piece.team != team:
+                    check_capture_condition = checkers_board.can_capture(move, opp_piece)
+                    if check_capture_condition is True:
+                        capturable_moves[move] = opp_piece.moves[move]
+    return capturable_moves
+
+
+def process_piece_selection(team, pieces):
+    selected_piece = select_piece(pieces)
+
+    capturable_moves = check_capturable_moves(selected_piece, team)
+    if capturable_moves:
+        capture_piece(capturable_moves, selected_piece)
+    else:
+        valid_moves = checkers_board.is_regular_move_valid(selected_piece)
+        if all(value is False for value in valid_moves.values()):
+            print("This piece has no valid moves. Select another piece")
+            process_piece_selection(team, pieces)  # Recusively restart turn
+        else:
+            make_valid_move(valid_moves, selected_piece)
+
+
 def player_turn(team, name):
     print(f"Player {name} Turn")
     if team == "white":
-        selected_piece = cycle_through_pieces(checkers_board.white_pieces)
+        # Add Logic to force capture before piece selection
+        process_piece_selection(team, checkers_board.white_pieces)
     elif team == "black":
-        selected_piece = cycle_through_pieces(checkers_board.black_pieces)
-       
-    print(selected_piece.name, selected_piece.xy_coord)
-    can_this_move = checkers_board.is_regular_move_valid(selected_piece) # checks direction of potential moves. returns dict
-    print("Debug for capture")
-    print(can_this_move)
-    
-    if any(value is False for value in can_this_move.values()): # If potential move is blocked checked why
-        capturable_move = {} # Make dict of all checked moves
-        for move in can_this_move:
-            print(f"move in can_this_move {move}")
-            if can_this_move[move] is False: # If move is illegal, check for piece
-                check_space = selected_piece.moves[move] # check_space is the (x, y) of a potential move space given by the moved piece
-                print(f"check space dode {check_space}")
-                if check_space is not None: # If piece exists check team
-                    opp_piece = checkers_board.get_from_location(check_space)
-                    print(f"opp piece {opp_piece}")
-                    if opp_piece.team != team: # If enemy, check for capture conditions
-                        check_capture_condition = checkers_board.can_capture(move, opp_piece)  # Return True/False if capturable
-                        if check_capture_condition is True: # If capturable, add move to list
-                            capturable_move[move] = opp_piece.moves[move]
-                        # else:  # If not capturable, add move with None value to represent no valid move
-                        #     capturable_move[move] = None
+        # Add Logic to force capture before piece selection
+        process_piece_selection(team, checkers_board.black_pieces)
 
-        print(f"These moves allow you to capture pieces {capturable_move}")
-        if capturable_move: # If dict is not empty proceed to capture
-            capture_piece(capturable_move, selected_piece)
-        else: # Change code here'
-            print("else block triggered")
-            valid_moves = checkers_board.is_regular_move_valid(selected_piece)
-            print(f"valid moves debug: {valid_moves}")
-            if all(value is False for value in valid_moves.values()):
-                print("this piece has no valid moves. select another piece")
-                player_turn(team, name)
-            else:
-                make_valid_move(valid_moves, selected_piece)
-    else:
-        valid_moves = checkers_board.is_regular_move_valid(selected_piece)
-        make_valid_move(valid_moves, selected_piece)
 
-    # print(checkers_board.get_state())
+def board_force_capture():
+    '''
+    I need to have the game force the player to capture
+    a piece if available and not give him a choice to select another
+    '''
+
 
 # Turn Order
 is_game_active = True
@@ -141,3 +151,50 @@ while is_game_active is True:
     player_2_name = "Wes"
     turn1 = player_turn(WHITE_TEAM, player_1_name)
     turn2 = player_turn(BLACK_TEAM, player_2_name)
+
+
+
+"""
+Old player_turn code before abstraction
+"""
+
+# def player_turn(team, name):
+#     print(f"Player {name} Turn")
+#     if team == "white":
+#         selected_piece = cycle_through_pieces(checkers_board.white_pieces)
+#     elif team == "black":
+#         selected_piece = cycle_through_pieces(checkers_board.black_pieces)
+       
+#     print(selected_piece.name, selected_piece.xy_coord)
+#     can_this_move = checkers_board.is_regular_move_valid(selected_piece) # checks direction of potential moves. returns dict
+#     print("Debug for capture")
+#     print(can_this_move)
+    
+#     if any(value is False for value in can_this_move.values()): # If potential move is blocked checked why
+#         capturable_move = {} # Make dict of all potential capturable moves
+#         for move in can_this_move:
+#             print(f"move in can_this_move {move}")
+#             if can_this_move[move] is False: # If move is illegal, check for piece
+#                 check_space = selected_piece.moves[move] # check_space is the (x, y) of a potential move space given by the moved piece
+#                 print(f"check space dode {check_space}")
+#                 if check_space is not None: # If piece exists check team
+#                     opp_piece = checkers_board.get_from_location(check_space)
+#                     print(f"opp piece {opp_piece}")
+#                     if opp_piece.team != team: # If enemy, check for capture conditions
+#                         check_capture_condition = checkers_board.can_capture(move, opp_piece)  # Return True/False if capturable
+#                         if check_capture_condition is True: # If capturable, add move to list
+#                             capturable_move[move] = opp_piece.moves[move]
+
+#         print(f"These moves allow you to capture pieces {capturable_move}")
+#         if capturable_move:  # If dict is not empty proceed to capture
+#             capture_piece(capturable_move, selected_piece)
+#         else:
+#             valid_moves = checkers_board.is_regular_move_valid(selected_piece)
+#             if all(value is False for value in valid_moves.values()):
+#                 print("This piece has no valid moves. Select another piece")
+#                 player_turn(team, name)
+#             else:
+#                 make_valid_move(valid_moves, selected_piece)
+#     else:
+#         valid_moves = checkers_board.is_regular_move_valid(selected_piece)
+#         make_valid_move(valid_moves, selected_piece)
